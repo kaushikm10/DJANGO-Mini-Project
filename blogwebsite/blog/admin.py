f4c049cd68ea945aacef80d0982f529e758d0d3f
kaushikm10 <61223907+kaushikm10@users.noreply.github.com>
2020-12-11 11:20:49 +0530

    Minor Changes

--- blogwebsite/blog/admin.py
+++ blogwebsite/blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Blogger, Comments, Followers, Post
+from .models import Blogger, Post
 # Register your models here.
 
 admin.site.register(Blogger)
-admin.site.register(Comments)
-admin.site.register(Followers)
 admin.site.register(Post)
